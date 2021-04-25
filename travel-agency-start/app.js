80f679a6cd56b114225088bb914f67655dedb338
Kristof Matuszka <matuszkak@gmail.com>
2021-04-25 22:19:45 +0200

    js added - timer

--- travel-agency-start/app.js
+++ travel-agency-start/app.js
@@ -27,3 +27,27 @@ window.addEventListener('scroll', function () {
     document.querySelector('header').classList.remove('darken');
   }
 });
+
+const endValue = 567;
+const counter = document.querySelector('.counter');
+let alreadyPlayed = false;
+let currentValue = 0;
+
+const observer = new IntersectionObserver(function (entries) {
+  if (entries[0].isIntersecting === true && alreadyPlayed === false) {
+    console.log('element has just become visible on the screen');
+    alreadyPlayed = true;
+    const timer = setInterval(function () {
+      if (currentValue >= endValue) {
+        clearInterval(timer);
+      }
+      counter.innerText = currentValue;
+      currentValue = currentValue + 1;
+    }, 2) // 2 milliszekundum
+  }
+
+});
+
+observer.observe(counter);
+
+
